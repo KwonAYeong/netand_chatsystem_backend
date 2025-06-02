ac1416fbd7965bf870c0c232dd2174eb6a2c50e6
(hyungkey) <(hyungkey0601@gmail.com)>
2025-06-02 22:28:38 +0900

    feat:초기환경세팅 및 각 도메인 엔터티생성

---
+++ src/main/java/com/netand/chatsystem/chat/entity/ChatRoom.java
@@ -0,0 +1,25 @@
+package com.netand.chatsystem.chat.entity;
+
+import com.netand.chatsystem.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "chat_room")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ChatRoom extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String chatRoomName;
+
+    @Column(nullable = false)
+    private String chatRoomType;  // "DM", "GROUP"
+
+}
---
+++ src/main/java/com/netand/chatsystem/user/entity/User.java
@@ -0,0 +1,35 @@
+package com.netand.chatsystem.user.entity;
+
+import com.netand.chatsystem.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String password;
+
+    private String profileImageUrl;
+
+    private String company;
+
+    private String rank;
+
+    private boolean isActive;
+
+}
