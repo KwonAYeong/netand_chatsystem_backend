f6fb6202358aee6516443b3d63b0bf5fd8790c09
KwonAYeong <129161150+KwonAYeong@users.noreply.github.com>
2025-06-13 23:37:17 +0900

    fix: WebSocket 메세지 중복 수신 문제 해결

--- src/main/java/com/netand/chatsystem/chat/repository/ChatRoomParticipantRepository.java
+++ src/main/java/com/netand/chatsystem/chat/repository/ChatRoomParticipantRepository.java
@@ -1,11 +1,13 @@
 package com.netand.chatsystem.chat.repository;
 
-import com.netand.chatsystem.chat.dto.ChatRoomListResponseDTO;
 import com.netand.chatsystem.chat.entity.ChatRoom;
 import com.netand.chatsystem.chat.entity.ChatRoomParticipant;
 import com.netand.chatsystem.user.entity.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,8 +16,10 @@ public interface ChatRoomParticipantRepository extends JpaRepository<ChatRoomPar
 
     List<ChatRoomParticipant> findByUserId(Long userId);
 
-    Optional<ChatRoomParticipant> findByChatRoomAndUser(ChatRoom chatRoom, User user);
-
     List<ChatRoomParticipant> findByChatRoomId(Long chatroomId);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM ChatRoomParticipant p WHERE p.chatRoom.id = :chatRoomId AND p.user.id = :userId")
+    Optional<ChatRoomParticipant> findWithLockByChatRoomIdAndUserId(@Param("chatRoomId") Long chatRoomId,
+                                                                    @Param("userId") Long userId);
 }
