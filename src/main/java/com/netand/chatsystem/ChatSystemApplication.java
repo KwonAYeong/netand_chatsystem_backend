4a1f6d3158a6e9004bc70351bf2e4f845e6d3534
KwonAYeong <129161150+KwonAYeong@users.noreply.github.com>
2025-06-28 02:38:13 +0900

    feat: 서버시간 KST 기준으로 저장되도록 수정

--- src/main/java/com/netand/chatsystem/ChatSystemApplication.java
+++ src/main/java/com/netand/chatsystem/ChatSystemApplication.java
@@ -3,19 +3,16 @@ package com.netand.chatsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.ZonedDateTime;
 import java.util.TimeZone;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ChatSystemApplication {
 
-	public static void main(String[] args) {
-		// 타임존 및 시간 로그 찍기
-		System.out.println("서버 현재 시간: " + ZonedDateTime.now());
-		System.out.println("서버 타임존: " + TimeZone.getDefault().getID());
-
-		SpringApplication.run(ChatSystemApplication.class, args);
+	public static void main(String[] args) { SpringApplication.run(ChatSystemApplication.class, args);
 	}
 
 }
--- src/main/java/com/netand/chatsystem/common/BaseTimeEntity.java
+++ src/main/java/com/netand/chatsystem/common/BaseTimeEntity.java
@@ -1,34 +1,27 @@
 package com.netand.chatsystem.common;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @MappedSuperclass
 @Getter
 public abstract class BaseTimeEntity {
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     @Column(updatable = false)
-    @CreatedDate
     protected LocalDateTime createdAt;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
-    @LastModifiedDate
     protected LocalDateTime updatedAt;
 
     @PrePersist
     protected void onCreate() {
-        this.createdAt = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
         this.updatedAt = this.createdAt;
     }
 
     @PreUpdate
     protected void onUpdate() {
-        this.updatedAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
     }
 }
