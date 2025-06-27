a15552858fdca89c2793b7456095c05dc4ab4801
KwonAYeong <129161150+KwonAYeong@users.noreply.github.com>
2025-06-28 01:24:47 +0900

    fix: createdAt/updatedAt 응답 시간대를 Asia/Seoul 기준으로 설정

--- src/main/java/com/netand/chatsystem/common/BaseTimeEntity.java
+++ src/main/java/com/netand/chatsystem/common/BaseTimeEntity.java
@@ -1,16 +1,24 @@
 package com.netand.chatsystem.common;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
 public abstract class BaseTimeEntity {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     @Column(updatable = false)
+    @CreatedDate
     protected LocalDateTime createdAt;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+    @LastModifiedDate
     protected LocalDateTime updatedAt;
 
     @PrePersist
